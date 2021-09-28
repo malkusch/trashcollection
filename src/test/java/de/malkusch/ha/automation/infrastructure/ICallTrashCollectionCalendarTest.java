854f95efe18c908fd86ba20917707de89471fbc1
Markus Malkusch <markus@malkusch.de>
2021-09-28 09:40:50 +0200

    Add tests

--- src/test/java/de/malkusch/ha/automation/infrastructure/ICallTrashCollectionCalendarTest.java
+++ src/test/java/de/malkusch/ha/automation/infrastructure/ICallTrashCollectionCalendarTest.java
@@ -3,6 +3,7 @@ package de.malkusch.ha.automation.infrastructure;
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toSet;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -12,6 +13,7 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import de.malkusch.ha.automation.model.TrashCan;
 import de.malkusch.ha.automation.model.TrashCollectionCalendar;
@@ -40,6 +42,16 @@ public class ICallTrashCollectionCalendarTest {
         var trashCans = calendar.findTrashCollection(date);
 
         assertEquals(set(expectedCans), trashCans);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "2017-05-25", "2017-05-27" })
+    public void shouldNotFindTrashCollection(String dateString) {
+        var date = LocalDate.parse(dateString);
+
+        var trashCans = calendar.findTrashCollection(date);
+
+        assertTrue(trashCans.isEmpty());
     }
 
     private static Set<TrashCan> set(String cans) {
