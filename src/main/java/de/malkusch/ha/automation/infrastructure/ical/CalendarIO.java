15b5f3c0937ee5ade2b6daa8c136459d930c1a9b
Markus Malkusch <markus@malkusch.de>
2023-01-05 01:53:18 +0100

    Print calendar name

--- src/main/java/de/malkusch/ha/automation/infrastructure/ical/CalendarIO.java
+++ src/main/java/de/malkusch/ha/automation/infrastructure/ical/CalendarIO.java
@@ -1,5 +1,8 @@
 package de.malkusch.ha.automation.infrastructure.ical;
 
+import static net.fortuna.ical4j.model.Property.PRODID;
+import static net.fortuna.ical4j.model.Property.VERSION;
+
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
@@ -7,6 +10,7 @@ import java.time.Instant;
 import lombok.extern.slf4j.Slf4j;
 import net.fortuna.ical4j.data.ParserException;
 import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.Property;
 
 @Slf4j
 public class CalendarIO implements AutoCloseable {
@@ -33,6 +37,7 @@ public class CalendarIO implements AutoCloseable {
             lastUpdate = file.lastUpdate();
             log.warn("Calendar recovered from {}", lastUpdate);
         }
+        log.info("Loaded calender {}", calendarName(calendar));
     }
 
     public Calendar fetch() {
@@ -43,13 +48,25 @@ public class CalendarIO implements AutoCloseable {
         }
 
         try {
-            calendar = http.download();
+            var downloaded = http.download();
+            if (!calendarName(calendar).equals(calendarName(downloaded))) {
+                log.info("Calender {} updated to {}", calendarName(calendar), calendarName(downloaded));
+            }
+            calendar = downloaded;
             lastUpdate = Instant.now();
 
         } catch (IOException | InterruptedException | ParserException e) {
             log.warn("Failed to download calendar", e);
         }
         return calendar;
+    }
+
+    private String calendarName(Calendar calendar) {
+        return String.format("V %s, %s, %s", //
+                calendar.getProperty(VERSION).map(Property::getValue).orElse(""), //
+                calendar.getProperty(PRODID).map(Property::getValue).orElse(""), //
+                calendar.getProperty("X-WR-CALDESC").map(Property::getValue).orElse("") //
+        );
     }
 
     @Override
