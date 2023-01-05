7babc0736f7d05784ed0321a8e58a6eb6178c3c2
Markus Malkusch <markus@malkusch.de>
2023-01-05 01:10:09 +0100

    Refactor

---
+++ src/main/java/de/malkusch/ha/automation/application/ApplicationServiceSchedules.java
@@ -0,0 +1,35 @@
+package de.malkusch.ha.automation.application;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import de.malkusch.ha.automation.model.CheckTrashDayService;
+import de.malkusch.ha.automation.model.NextTrashCollection;
+import lombok.RequiredArgsConstructor;
+
+public class ApplicationServiceSchedules {
+
+    @Service
+    @RequiredArgsConstructor
+    public static final class CheckNextTrashCollectionSchedule {
+
+        private final NextTrashCollection next;
+
+        @Scheduled(cron = "59 49 5 * * *")
+        public void checkNextChanged() {
+            next.checkNextChanged();
+        }
+    }
+
+    @Service
+    @RequiredArgsConstructor
+    public final class CheckTrashDaySchedule {
+
+        private final CheckTrashDayService checker;
+
+        @Scheduled(cron = "59 59 5 * * *")
+        public void checkTrashDay() {
+            checker.checkTomorrow();
+        }
+    }
+}
