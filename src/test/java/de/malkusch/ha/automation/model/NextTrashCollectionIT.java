9aaf939728ac19b26c2e73fe087bfd7d982c7803
Markus Malkusch <markus@malkusch.de>
2023-01-05 02:35:49 +0100

    Fix tests

--- src/test/java/de/malkusch/ha/automation/model/NextTrashCollectionIT.java
+++ src/test/java/de/malkusch/ha/automation/model/NextTrashCollectionIT.java
@@ -5,15 +5,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+import de.malkusch.ha.Application;
+
+@SpringBootTest(classes = Application.class)
 @ActiveProfiles(profiles = "test")
-@ExtendWith(MockitoExtension.class)
 public class NextTrashCollectionIT {
 
     @Autowired
