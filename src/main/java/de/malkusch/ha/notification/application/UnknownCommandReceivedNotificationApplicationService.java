89720695f84fc09849bd7f2011d2d56389a2d5c2
Markus Malkusch <markus@malkusch.de>
2024-01-24 10:11:44 +0100

    Ignore

--- src/main/java/de/malkusch/ha/notification/application/UnknownCommandReceivedNotificationApplicationService.java
+++ src/main/java/de/malkusch/ha/notification/application/UnknownCommandReceivedNotificationApplicationService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import de.malkusch.ha.notification.model.Notification;
 import de.malkusch.ha.notification.model.NotificationService;
 import de.malkusch.ha.shared.infrastructure.telegram.CommandDispatcher.UnkownCommandReceived;
+import de.malkusch.ha.shared.infrastructure.telegram.CommandParser.ReactionMessage;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -16,6 +17,9 @@ public final class UnknownCommandReceivedNotificationApplicationService {
 
     @EventListener
     public void onUnknown(UnkownCommandReceived event) {
+        if (event.command() instanceof ReactionMessage) {
+            return;
+        }
         var message = String.format("Unbekannter Befehl: %s\n\n%s", event.command(), event.help());
         var notification = new Notification(message);
         notificationService.send(notification);
